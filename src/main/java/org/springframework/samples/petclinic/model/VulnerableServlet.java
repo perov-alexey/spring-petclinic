f2ceecb98b2755ab76c0332cf19433bbec1b733a
Alexey Perov <perov-alexey@yandex.ru>
2018-12-05 16:02:04 +0300

    Add more vulnerable code

--- src/main/java/org/springframework/samples/petclinic/model/VulnerableServlet.java
+++ src/main/java/org/springframework/samples/petclinic/model/VulnerableServlet.java
@@ -1,11 +1,17 @@
 package org.springframework.samples.petclinic.model;
 
 import javax.servlet.http.HttpServlet;
+import java.io.File;
+import java.io.IOException;
 
 public class VulnerableServlet extends HttpServlet {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Vulnerability!");
+        File tempDir;
+        tempDir = File.createTempFile("", ".");
+        tempDir.delete();
+        tempDir.mkdir();  // Noncompliant
     }
 
 }
