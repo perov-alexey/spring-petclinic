a684da3c4f1e15a7aa9573eaba7e2877cb999a3d
Alexey Perov <perov-alexey@yandex.ru>
2018-12-10 19:08:06 +0300

    Add failing test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -42,10 +42,10 @@ public class ValidatorTests {
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
         assertThat(violation.getMessage()).isEqualTo("must not be empty");
     }
-//
-//    @Test
-//    public void failingTest() {
-//        assertThat("1").isEqualTo("2");
-//    }
+
+    @Test
+    public void failingTest() {
+        assertThat("1").isEqualTo("2");
+    }
 
 }
